fbd1ebf0005d2be614e49b19997ddd7d7c021922
mandani-tech <mandani.hamida@gmail.com>
2019-08-13 12:02:40 -0500

    class work complete

---
+++ index.js
@@ -0,0 +1,47 @@
+// #### Exercise 1
+// 1. Create an array.
+// 2. Add four names to the array.
+// 3. Print the array
+//
+let namesArray =["BOB","MIKE","TOM","STEVE"];
+console.log(namesArray);
+alert (namesArray[0]);
+alert (namesArray[1]);
+alert (namesArray[2]);
+alert (namesArray[3]);
+
+
+
+// #### Exercise 2
+// 1. Create an array with 5 numbers in it.
+// 2. Print the numbers.
+//
+ let numbersArray =[1,2,3,4,5];
+ console.log(namesArray);
+ alert(numbersArray[0]);
+ alert(numbersArray[1]);
+ alert(numbersArray[2]);
+ alert(numbersArray[3]);
+ alert(numbersArray[4]);
+
+
+//     #### Exercise 3
+// 1. Create an array with 4 names.
+// 2. Print the third item in the array.
+// 3. Delete the second item.
+// 4. Print the third item in the array again.
+
+
+
+let namesArray2 =["BOB","MIKE","TOM","STEVE"];
+console.log(namesArray2[2]);
+alert(namesArray2[2]); //Print the third item in the array.
+console.log(namesArray2)
+namesArray2.splice(2,1); // Delete the second item.
+alert(namesArray2[2]); //Print the third item in the array.
+console.log(namesArray2)
+
+
+
+
+
